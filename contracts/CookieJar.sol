d26babe544c3fa9985f3ba8ffb41e8e3765e4ff8
aorap-hackathon <a.orap@icloud.com>
2024-08-04 20:52:26 +0200

    Update CookieJar.sol
    done with EAS, starting adding Pyth

--- contracts/CookieJar.sol
+++ contracts/CookieJar.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
+import "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
 
 import { IEAS, Attestation, AttestationRequest, AttestationRequestData, RevocationRequest, RevocationRequestData } from "@ethereum-attestation-service/eas-contracts/contracts/IEAS.sol";
 import { SchemaRegistry, ISchemaRegistry, SchemaRecord } from "@ethereum-attestation-service/eas-contracts/contracts/SchemaRegistry.sol";
@@ -12,13 +14,37 @@ import { NO_EXPIRATION_TIME, EMPTY_UID } from "@ethereum-attestation-service/eas
 
 contract CookieJar is AccessControl, SchemaResolver {
     /* --------------------- EAS specific code begin --------------------- */
-    address private immutable resolverAddress;
     bytes32 public withdrawEasSchema;
     bytes32 public depositEasSchema;
     bytes32 public voteEasSchema;
+    // ~Delegated Attestations was an option but they are harder to implement. Let's just include who truly attested in an attestation itself.~
     string private constant withdrawSchema = "(address dao_member, uint256 withdraw_id, uint256 amount, string note)";
     string private constant depositSchema = "(address dao_member, uint256 deposit_id, uint256 amount, string note)";
     string private constant voteSchema = "(address dao_member, bool is_upvote)";
+
+    // ~DAO members only attestation.~
+    function onAttest(Attestation calldata attestation, uint256 /*value*/) internal override view returns (bool) {
+        return attestation.attester == address(this);
+    }
+
+    function onRevoke(Attestation calldata attestation, uint256 /*value*/) internal override view returns (bool) {
+        return attestation.attester == address(this);
+    }
+    /* --------------------- EAS specific code end --------------------- */
+
+
+    bytes32 public constant DAO_MEMBER_ROLE = keccak256("DAO_MEMBER_ROLE");
+
+    ERC20 public usdToken; // base mainnet USDC address 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913
+    address private constant _usdTokenAddress = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+    address private constant _easAddress = 0x4200000000000000000000000000000000000021;
+
+    address private constant _pythAddress = 0xA2aa501b19aff244D90cc15a4Cf739D2725B5729;
+    bytes32 private constant priceFeedId = 0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace; // ETH/USD
+    IPyth pyth;
+
+    uint256 public withdrawalLimit; // default is 100 USD
+    uint256 public cooldownPeriod; // use 28800 for 8 hours
 
     struct Withdrawal {
         address user;
@@ -40,25 +66,6 @@ contract CookieJar is AccessControl, SchemaResolver {
         bytes32 uid; // attestation uid
         bool isUpvote;
     }
-
-    function onAttest(Attestation calldata attestation, uint256 /*value*/) internal override view returns (bool) {
-        return attestation.attester == address(this);
-    }
-
-    function onRevoke(Attestation calldata attestation, uint256 /*value*/) internal override view returns (bool) {
-        return attestation.attester == address(this);
-    }
-    /* --------------------- EAS specific code end --------------------- */
-
-
-    bytes32 public constant DAO_MEMBER_ROLE = keccak256("DAO_MEMBER_ROLE");
-
-    ERC20 public usdToken; // base mainnet USDC address 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913
-    address private immutable _usdTokenAddress = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
-    address private immutable _easAddress = 0x4200000000000000000000000000000000000021;
-    uint256 public withdrawalLimit; // default is 100 USD
-    uint256 public cooldownPeriod; // use 28800 for 8 hours
-
 
     Withdrawal[] public withdrawals;
     Deposit[] public deposits;
@@ -92,18 +99,13 @@ contract CookieJar is AccessControl, SchemaResolver {
     ) SchemaResolver(IEAS(_easAddress)) {
         // require(_usdToken != address(0), "Usd token address cannot be zero");
 
-        // require(_withdrawEasSchema != bytes32(0), "Withdraw EAS schema cannot be zero");
-        // require(_depositEasSchema != bytes32(0), "Deposit EAS schema cannot be zero");
-        // require(_depositEasSchema != bytes32(0), "Withdraw and Deposit EAS schema cannot be equal");
-        // withdrawEasSchema = _withdrawEasSchema;
-        // depositEasSchema = _depositEasSchema;
-
-
-        resolverAddress = address(this);
+        // ~Register new schemes because the EAS resolver is contract itself.~
         ISchemaRegistry schemaRegistry = _eas.getSchemaRegistry();
         withdrawEasSchema = schemaRegistry.register(withdrawSchema, this, false);
         depositEasSchema = schemaRegistry.register(depositSchema, this, false);
         voteEasSchema = schemaRegistry.register(voteSchema, this, true);
+
+        pyth = IPyth(_pythAddress);
 
         usdToken = ERC20(_usdTokenAddress);
         withdrawalLimit = 100;
@@ -116,6 +118,35 @@ contract CookieJar is AccessControl, SchemaResolver {
 
     // TODO: Implement Pyth price feeds to specify amount in USD
     function withdrawNative(uint256 _amount, string memory _note) external onlyRole(DAO_MEMBER_ROLE) {
+        // require(block.timestamp >= lastWithdrawalTime[msg.sender] + cooldownPeriod, "Cooldown period not elapsed");
+        // require(_amount <= withdrawalLimit, "Amount exceeds withdrawal limit");
+
+        uint256 nativeBalance = address(this).balance;
+        require(nativeBalance >= _amount, "Insufficient funds in the jar");
+
+        payable(msg.sender).transfer(_amount);
+
+        bytes32 withdrawAttestUid = _eas.attest(
+            AttestationRequest({
+                schema: withdrawEasSchema,
+                data: AttestationRequestData({
+                    recipient: address(this), // Cookie Jar itself is recepient
+                    expirationTime: NO_EXPIRATION_TIME, // No expiration time
+                    revocable: false,
+                    refUID: EMPTY_UID, // No references UI
+                    data: abi.encode(msg.sender, withdrawals.length, _amount, _note),
+                    value: 0 // No value/ETH
+                })
+            })
+        );
+
+        lastWithdrawalTime[msg.sender] = block.timestamp;
+        withdrawals.push(Withdrawal(msg.sender, _amount, _note, block.timestamp, 0, withdrawAttestUid));
+        
+        emit WithdrawalMade(msg.sender, _amount, _note, withdrawals.length - 1, withdrawAttestUid);
+    }
+
+    function withdrawNativeWithPyth(uint256 _amount, string memory _note, bytes[] calldata priceUpdate) external onlyRole(DAO_MEMBER_ROLE) {
         // require(block.timestamp >= lastWithdrawalTime[msg.sender] + cooldownPeriod, "Cooldown period not elapsed");
         // require(_amount <= withdrawalLimit, "Amount exceeds withdrawal limit");
 
@@ -202,6 +233,7 @@ contract CookieJar is AccessControl, SchemaResolver {
     function voteOnWithdraw(uint256 _withdrawalId, bool _isUpvote) external onlyRole(DAO_MEMBER_ROLE) {
         require(_withdrawalId < withdrawals.length, "Invalid withdrawal ID");
 
+        // Remove last vote if exist.
         if (lastVote[msg.sender][_withdrawalId].uid != bytes32(0)) {
             Vote memory _lastVote = lastVote[msg.sender][_withdrawalId];
             require(_lastVote.isUpvote != _isUpvote, "Same vote again");
@@ -306,17 +338,8 @@ contract CookieJar is AccessControl, SchemaResolver {
         return usdToken.decimals();
     }
 
-    function getResolvedAddress() external view returns (address) {
-        return resolverAddress;
-    }
-
     function getWithdrawEasSchema() external view returns (bytes32) {
         return withdrawEasSchema;
-    }
-
-    function getVoteSchema() external view returns (SchemaRecord memory) {
-        ISchemaRegistry schemaRegistry = _eas.getSchemaRegistry();
-        return schemaRegistry.getSchema(voteEasSchema);
     }
     /* --------------------- Read functions end --------------------- */
 }
