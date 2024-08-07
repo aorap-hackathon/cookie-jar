228cda36b3f7407c3b323525b81d70edb791f00d
aorap-hackathon <a.orap@icloud.com>
2024-08-08 00:25:10 +0200

    adding wait for transaction receipt

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bab631d73336c592"; }
+/******/ 		__webpack_require__.h = function() { return "96edf43fdfcf867e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
